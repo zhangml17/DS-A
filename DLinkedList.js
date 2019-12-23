9452c93720890490dc1aca57e3c7133d33c96c2f
zhangmeilong <meilongyx@163.com>
2019-12-23 10:37:40 +0800

    add: DLinkedList

--- DLinkedList.js
+++ DLinkedList.js
@@ -1,8 +1,12 @@
-// 构造一个链表结构
+/**
+ * 双向链表结构
+ * 和单链表的区别：多了一个指向前驱节点的属性previous
+ */
 class Node {
     constructor(elem) {
         this.elem = elem
         this.next = null
+        this.previous = null
     }
 }
 
@@ -27,26 +31,45 @@ class LList {
         }
         return currentNode
     }
+    // 查找最后一个节点
+    findLast() {
+        let currentNode = this.head
+        while(currentNode.next != null) {
+            currentNode = currentNode.next
+        }
+        return currentNode
+    }
     // 删除一个节点
     remove(item) {
-        let prevNode = this.findPrevious(item)
-        // if(prevNode.next != null) {
-        prevNode.next = prevNode.next.next
-        // }
+        let currentNode = this.find(item)
+        currentNode.privious.next = currentNode.next
+        currentNode.next.privious = currentNode.privious
+        // 销毁被删除的节点
+        currentNode.previous = null
+        currentNode.next = null
     }
     // 在item节点之后插入一个节点newElem
     insert(newElem, elem) {
         let newNode = new Node(newElem)
-        let prevNode = this.find(elem)
-        newNode.next = prevNode.next
-        prevNode.next = newNode
+        let currentNode = this.find(elem)
+        newNode.next = currentNode.next
+        newNode.previous = currentNode
+        currentNode.next = newNode
     }
-    // 打印链表所有节点
+    // 顺序打印链表所有节点
     display() {
         let currentNode = this.head
         while(currentNode.next != null) {
             currentNode = currentNode.next
             console.log(currentNode.elem)
+        }
+    }
+    // 倒序打印链表所有节点
+    displayReverse() {
+        let currentNode = this.findLast()
+        while(currentNode.previous != null) {
+            console.log(currentNode.elem)
+            currentNode = currentNode.previous
         }
     }
 }
@@ -57,5 +80,7 @@ cities.insert('Shanghai', 'Beijing')
 cities.insert('Xian', 'Shanghai')
 cities.insert('Guangzhou', 'Xian')
 cities.insert('Hangzhou', 'Guangzhou')
-cities.remove('Xian')
+// cities.remove('Xian')
 cities.display()
+console.log('--------我是分割线--------')
+cities.displayReverse()
